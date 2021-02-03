4753229570dd7a7567128d7072260615cc62ed84
HERICK ANDERSON DOS SANTOS <20272523@cfp127aluno.local>
2021-02-03 11:22:12 -0300

    ProjetoAcademia 03/02/2021

---
+++ academia/src/br/senai/sp/jandira/Projetoacademia/Cliente.java
@@ -0,0 +1,45 @@
+package br.senai.sp.jandira.Projetoacademia;
+
+import java.time.LocalDate;
+
+public class Cliente {
+
+	private String nome;
+	private LocalDate datadenascimento;
+	private char sexo;
+	private double peso;
+	private double altura;
+	private int niveldeatividade;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ academia/src/br/senai/sp/jandira/model/Projetoacademia/App.java
@@ -0,0 +1,55 @@
+package br.senai.sp.jandira.model.Projetoacademia;
+
+import br.senai.sp.jandira.Projetoacademia.Cliente;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println();
+		// TODO Auto-generated method stub
+
+		Cliente clienteMarcos = new Cliente ();
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
