93a77dfd99ed94d278da823463d9ca47160bda44
OChriiis <herickandersondossantos012342hotmail.com>
2021-02-11 15:58:21 -0300

    https://github.com/OChriiis/projetoacademia

--- academia/src/br/senai/sp/jandira/Projetoacademia/Cliente.java
+++ academia/src/br/senai/sp/jandira/Projetoacademia/Cliente.java
@@ -69,13 +69,31 @@ public class Cliente {
 	public void setIMC(double IMC){
 	 return this.IMC
 	 }
-	 public double getIMC(){
+	 public double getCalcularIMC(){
 	 return this.peso/(this.altura*this.altura)
 	 
-	 
-	 
-	 
 	 }
+	   
+	 
+	 if (this.getCalcularIMC() <= 18.5) {
+		return "Abaixo do peso!";
+		}
+		else if (this.getCalcularIMC() >= 18.6 && <=24.9){
+			return "Peso ideal, parabéns!";
+		}
+		else if (this.getCalcularIMC() >=25.0 && <=29.9 ){
+			return "Levemente acima do peso!";
+		}
+		else if (this.getCalcularIMC() >=30.0 && <= 34.9){
+			return "Obesidade grau I!";
+		}
+		else if (this.getCalcularIMC() >=35.0 && <= 39.5){
+		  return "Obesidade grau II(Severa)";
+		 }
+		else (this.getCalcularIMC() >=40){
+			return "Obesidade III (Mórbida)";
+		}
+	 
 	
 	  
   
