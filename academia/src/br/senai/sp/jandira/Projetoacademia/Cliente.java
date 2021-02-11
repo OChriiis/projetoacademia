1474bc27701ec368d1016d4389e99de10df5ec0c
OChriiis <herickandersondossantos012342hotmail.com>
2021-02-11 14:05:06 -0300

    https://github.com/OChriiis/projetoacademia

--- academia/src/br/senai/sp/jandira/Projetoacademia/Cliente.java
+++ academia/src/br/senai/sp/jandira/Projetoacademia/Cliente.java
@@ -13,23 +13,71 @@ public class Cliente {
 	
 	public void setNome(String nome) {
 		
-	return this.nome;
+	this.nome = nome;
+	}
+	 public void setDataDeNascimento(LocalDate datadenascimento) {
+
+	 this.datadenascimento = datadenascimento;
+	 }
+	public void setSexo(char sexo) {
+
+	this.sexo = sexo;
+	}
+	public void setPeso(double peso){
+
+	 this.peso = peso;
+	}
+	public void setAltura(double altura){
+
+	this.altura = altura;
+	}
+	public void setNivelDeAtividade (int niveldeatividade){
+
+	 this.niveldeatividade = niveldeatividade;
 	}
 	
+	public String getNome() {
+		return this.nome;
+	}
 	
+	public LocalDate getDataDeNascimento() {
+		return this.datadenascimento;
+	}
+	 public int getIdade(){
+		LocalDate hoje = LocalDate.now():
+		Period period = Period.between(this.datadenascimento, hoje);
+		return period.getYears();
+
+	 }
 	
+	 public char getSexo(){
+		  return this.sexo;
+	 }
 	
+	public double getPeso(){
+		return this.peso;
+	}
 	
+	public double getAltura(){
+		return this.altura;
+	}
 	
-	
+	public int getNivelDeAtividade(){
+		return this.niveldeatividade;
+	}
 	
 	public void setIMC(double IMC){
-	 Return this.IMC
+	 return this.IMC
 	 }
 	 public double getIMC(){
-	 Return this.peso/(this.altura*this.altura)
+	 return this.peso/(this.altura*this.altura)
+	 
+	 
+	 
+	 
 	 }
 	
+	  
   
 	
 	
