9b6bcb9361e8d6f3291d7a5b442dce4ebc63f7e7
OChriiis <herickandersondossantos012342hotmail.com>
2021-02-12 10:46:33 -0300

    https://github.com/OChriiis/projetoacademia

---
+++ academia/src/br/senai/sp/jandira/model/Projetoacademia/Nivelatividade.java
@@ -0,0 +1,14 @@
+ 
+public class NivelDeAtividade {
+
+//Homem
+	public static final double leve =(1.5);
+	public static final double moderado =(1.8);
+	public static final double intenso =(2.1);
+
+//Muié
+	public static final double leve =(1.6);
+	public static final double moderado =(1.6);
+	public static final double intenso =(1.8);
+
+}
